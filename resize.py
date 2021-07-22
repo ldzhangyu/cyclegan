aff59996d5a5e51126a6a402e1c786f0f0d3fea5
ldzhangyu <ldzhangyu@126.com>
2021-07-22 17:36:39 +0800

    修改图片大小

--- resize.py
+++ resize.py
@@ -8,7 +8,7 @@ import argparse
 
 
 
-data_path = '/Users/zhangyu/python/cyclegan/datasets/bitmoji/testOne'
+data_path = '/Users/zhangyu/python/cyclegan/datasets/female/testA'
 
 for idx, img_name in enumerate(tqdm(os.listdir(data_path))):
     img = cv2.imread(os.path.join(data_path, img_name))
@@ -18,7 +18,7 @@ for idx, img_name in enumerate(tqdm(os.listdir(data_path))):
         size = (256, 256)
         newImg = cv2.resize(img, size)
         # cv2.imwrite(os.path.join((args.save_path), img_name), newImg)
-        cv2.imwrite(data_path+'/new_'+ img_name, newImg)
+        cv2.imwrite(data_path+'/new/'+ img_name, newImg)
         print('write ',img_name)
 
 
